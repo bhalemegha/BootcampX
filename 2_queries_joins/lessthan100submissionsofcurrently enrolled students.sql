a5608bf0e1e40674aa7d85a2ecfe724a2b7d1d62
bhalemegha <meghabhale6@gmail.com>
2021-11-23 00:06:19 +0000

    Join queries

---
+++ 2_queries_joins/lessthan100submissionsofcurrently enrolled students.sql
@@ -0,0 +1,6 @@
+SELECT students.name as student,students.end_date, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+GROUP BY students.name, students.end_date
+HAVING students.end_date IS NULL
+AND count(assignment_submissions.*) < 100;
