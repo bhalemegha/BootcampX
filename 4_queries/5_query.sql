8774c0477f8a48580fc8305f6f79dd1d20e9056f
bhalemegha <meghabhale6@gmail.com>
2021-11-23 21:31:55 +0000

    average requests assistance time, waiting time and few more

---
+++ 4_queries/5_query.sql
@@ -0,0 +1,8 @@
+SELECT c.name AS name, AVG(completed_at - started_at) AS average_assistance_time
+FROM assistance_requests AS a
+JOIN students AS s
+ON s.id = a.student_id
+JOIN cohorts AS c
+ON c.id = s.cohort_id
+GROUP BY c.name
+ORDER BY average_assistance_time;
